957bd577f554980f599a49bfd01fc6535c1c2424
rriver52 <rriver52>
2022-03-06 12:27:34 -0600

    changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,7 +13,7 @@ const loggingOut = () => {
 </script>
 
 <template>
-  <div class="text-green-200 bg-blue-800">
+  <div class="text-red-200 bg-pink-800">
     <div class="container flex items-center justify-between mx-auto">
       <h1 class="text-3xl font-thin tracking-tighter">
         Vue<span class="font-normal">RealAuth</span>
